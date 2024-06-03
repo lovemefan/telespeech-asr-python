e4540d556742e25abe1c01d4d2fba76c8c455b03
lovemefan <lovemefan@outlook.com>
2024-06-03 16:38:50 +0800

    add int8 onnx export

--- telespeechasr/onnx/onnx_export.py
+++ telespeechasr/onnx/onnx_export.py
@@ -31,19 +31,21 @@ def get_parser():
         required=True,
         help="Output dir of model checkpoint",
     )
+    parser.add_argument(
+        "--quantize",
+        action="store_true",
+        help="Whether to quantize the model",
+    )
     args = parser.parse_args()
     return args
 
 
-if __name__ == "__main__":
-    args = get_parser()
-    model = Data2VecMultiModel()
-    model = load_checkpoint(args.model_path, model)
-    model_export = data2vec_multo_model_export(model)
+def export_onnx(args, model):
+    model_path = os.path.join(args.output_dir, f"model_export.onnx")
     torch.onnx.export(
-        model_export,
+        model,
         (torch.randn(1, 155, 40)),
-        os.path.join(args.output_dir, f"model_export.onnx"),
+        model_path,
         verbose=False,
         opset_version=11,
         input_names=["feats"],
@@ -53,3 +55,27 @@ if __name__ == "__main__":
             "encoder_out": {1: "T"},
         },
     )
+    if args.quantize:
+        import onnx
+        from onnxruntime.quantization import QuantType, quantize_dynamic
+
+        quant_model_path = os.path.join(
+            args.output_dir, f"model_export_int8_quant.onnx"
+        )
+        if not os.path.exists(quant_model_path):
+            quantize_dynamic(
+                model_input=model_path,
+                model_output=quant_model_path,
+                op_types_to_quantize=["MatMul"],
+                per_channel=True,
+                reduce_range=False,
+                weight_type=QuantType.QUInt8,
+            )
+
+
+if __name__ == "__main__":
+    args = get_parser()
+    model = Data2VecMultiModel()
+    model = load_checkpoint(args.model_path, model)
+    model_export = data2vec_multo_model_export(model)
+    export_onnx(args, model_export)
